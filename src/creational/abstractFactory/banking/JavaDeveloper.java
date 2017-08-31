4696aefaa688931e317ddaf14428223b2402dbc8
Alexander <alexandr-ryabykh@yander.ru>
2017-08-31 20:13:44 +0300

    creational patterns

--- src/creational/abstractFactory/banking/JavaDeveloper.java
+++ src/creational/abstractFactory/banking/JavaDeveloper.java
@@ -2,7 +2,7 @@ package creational.abstractFactory.banking;
 
 import creational.abstractFactory.Developer;
 
-public class JavaDeveloper implements Developer{
+public class JavaDeveloper implements Developer {
     @Override
     public void writeCode() {
         System.out.println("Java dev coding");
--- src/creational/abstractFactory/website/ManualTester.java
+++ src/creational/abstractFactory/website/ManualTester.java
@@ -2,7 +2,7 @@ package creational.abstractFactory.website;
 
 import creational.abstractFactory.Tester;
 
-public class ManualTester implements Tester{
+public class ManualTester implements Tester {
     @Override
     public void testCode() {
         System.out.println("Manual tester is testing");
