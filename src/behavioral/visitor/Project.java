fd0eb66b967a1fdfab04af3f7bb2be025b1023fd
Alexander <alexandr-ryabykh@yander.ru>
2017-08-31 15:16:36 +0300

    behavioral patterns

---
+++ src/behavioral/visitor/Project.java
@@ -0,0 +1,20 @@
+package behavioral.visitor;
+
+public class Project implements ProjectElement {
+    private ProjectElement[] projectElements;
+
+    Project() {
+        this.projectElements = new ProjectElement[]{
+                new ProjectClass(),
+                new Database(),
+                new Test()
+        };
+    }
+
+    @Override
+    public void beWritten(Developer developer) {
+        for (ProjectElement projectElement : projectElements) {
+            projectElement.beWritten(developer);
+        }
+    }
+}
