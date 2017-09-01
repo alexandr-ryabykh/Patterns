b1d968cd7c02578880e6d8d75971ac73acfca095
Alexander <alexandr-ryabykh@yander.ru>
2017-09-01 11:32:27 +0300

    creational patterns

---
+++ src/creational/prototype/ProjectFactory.java
@@ -0,0 +1,18 @@
+package creational.prototype;
+
+public class ProjectFactory {
+    private Project project;
+
+    ProjectFactory(Project project) {
+        this.project = project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    Project cloneProject() {
+        return (Project) project.copy();
+    }
+
+}
