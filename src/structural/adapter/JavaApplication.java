8afbd1fdc663156f6b281adbcd736f81f1a169c6
Alexander <alexandr-ryabykh@yander.ru>
2017-09-04 10:37:01 +0300

    structural patterns

---
+++ src/structural/adapter/JavaApplication.java
@@ -0,0 +1,19 @@
+package structural.adapter;
+
+class JavaApplication {
+    void saveObject() {
+        System.out.println("Saving Java object ...");
+    }
+
+    void updateObject() {
+        System.out.println("Updating Java object ...");
+    }
+
+    void loadObject() {
+        System.out.println("Loading Java object ...");
+    }
+
+    void deleteObject() {
+        System.out.println("Deleting Java object ...");
+    }
+}
