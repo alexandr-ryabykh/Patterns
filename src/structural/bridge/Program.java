323016d1423a6dc15534187fffe4cf2f54277636
Alexander <alexandr-ryabykh@yander.ru>
2017-09-04 14:00:00 +0300

    structural patterns

---
+++ src/structural/bridge/Program.java
@@ -0,0 +1,11 @@
+package structural.bridge;
+
+public abstract class Program {
+    protected Developer developer;
+
+    Program(Developer developer) {
+        this.developer = developer;
+    }
+
+    public abstract void developProgram();
+}
