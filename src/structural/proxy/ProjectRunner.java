edef57afb933013f65c57e378a5549cb98c8382e
Alexander <alexandr-ryabykh@yander.ru>
2017-09-05 19:30:53 +0300

    structural patterns

--- src/structural/proxy/ProjectRunner.java
+++ src/structural/proxy/ProjectRunner.java
@@ -2,7 +2,7 @@ package structural.proxy;
 
 public class ProjectRunner {
     public static void main(String[] args) {
-        Project project = new ProxyProject("https://www.github.com/Patterns");
+        Project project = new ProxyProject("https://github.com/alexandr-ryabykh/Patterns");
         project.run();
     }
 }
